522ac63530d8f0548496202f8ef3a381866f1dec
Matt Sadauckas <matthewsadauckas@gmail.com>
2017-01-25 22:26:48 -0700

    feat(core) Changed served message

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('\n\nHello World')
+  res.send('\n\nThis is my very first web server!')
 });
 
 module.exports = app
